170103cf896c72c19ce98990a2c583579e95a5a2
abdalla-wasaa <abdallawasaa7@gmail.com>
2024-03-19 15:00:31 +0300

    First Attempt

--- server/app.py
+++ server/app.py
@@ -24,6 +24,108 @@ db.init_app(app)
 def index():
     return '<h1>Code challenge</h1>'
 
+@app.route('/restaurants')
+def restaurants():
+
+    restaurants = []
+    for restaurant in Restaurant.query.all():
+        restaurant_dict = {
+            "address": restaurant.address,
+            "id": restaurant.id,
+            "name": restaurant.name,
+        }
+        restaurants.append(restaurant_dict)
+
+    response = make_response(
+        restaurants,
+        200
+    )
+
+    return response
+
+@app.route('/restaurants/<int:id>',methods=['GET','DELETE'])
+def restaurant_by_id(id):
+    restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+    
+    if method == 'GET':
+        restaurant_dict = restaurant.to_dict()
+        response = make_response(
+            restaurant_dict,
+            200
+        )
+
+        return response
+
+    elif method == 'DELETE':
+        db.session.delete(restaurant)
+        db.session.commit()
+        
+        response_body={
+            "delete_successful": True,
+            "message": "Restaurant deleted."
+        }
+        response = make_response(
+            response_body,
+            200
+        )
+
+        return response
+
+@app.route('/pizzas')
+def pizzas():
+
+    pizzas = []
+    for pizza in Pizza.query.all():
+        pizza_dict = {
+            "id": pizza.id,
+            "ingredients": pizza.ingredients,
+            "name": pizza.name,
+        }
+        pizzas.append(pizza_dict)
+
+    response = make_response(
+        pizzas,
+        200
+    )
+
+    return response
+
+@app.route('/restaurant_pizzas',methods=['GET','POST'])
+def restaurant_pizzas():
+
+    if request.method == 'GET': 
+        restaurant_pizzas = []
+        for restaurant_pizza in RestaurantPizza.query.all():
+            restaurant_pizza_dict = restaurant_pizza.to_dict()
+            restaurant_pizzas.append(restaurant_pizza_dict)
+
+        response = make_response(
+            restaurant_pizzas,
+            200
+        )
+
+        return response
+    elif request.method == 'POST':
+        new_restaurant_pizza = RestaurantPizza(
+            price = request.form.get("price"),
+            price_id = request.form.get("price_id"),
+            restaurant_id = request.form.get("restaurant_id"),
+        )
+        db.session.add(new_restaurant_pizza)
+        db.session.commit()
+
+        restaurant_pizza_dict = new_restaurant_pizza.to_dict()
+        response = make_response(
+            restaurant_pizza_dict,
+            200
+        )
+        return response
+
+
+
+
+    
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
